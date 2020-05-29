059a54cc5f214d72d7596fd356ebe7828fbd0ee9
Vitold S <vit1251@gmail.com>
2020-05-29 12:43:58 +0300

    Check execution

--- tasks.py
+++ tasks.py
@@ -1,7 +1,14 @@
 
 from invoke import task
 
-@task(default=True)
-def build(c):
+@task
+def prepare(c):
     c.run('meson builddir', echo=True, pty=True)
+
+@task(default=True, pre=[prepare])
+def build(c):
     c.run('ninja -C builddir', echo=True, pty=True)
+
+@task(pre=[prepare])
+def check(c):
+    c.run('ninja -C builddir test', echo=True, pty=True)
